eb2687520e393e2d1eb6af44439ec000a3637ede
iambrianna <iambrianna@proton.me>
2023-07-11 23:28:10 +0200

    Added path autocompletion for gpx-concatenator command

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,18 @@
 import os
 import argparse
 from gpx_concatenator.gpx_concatenator import GPXConcatenator
+import argcomplete
 
 def main():
     # Create an argument parser
     parser = argparse.ArgumentParser(description='GPX Concatenator')
-    parser.add_argument('-i', '--input-dir', default='input', help='Directory containing input files')
-    parser.add_argument('-o', '--output-file', default='output.gpx', help='Output file name')
+    parser.add_argument('-i', '--input-dir', default='input', help='Directory containing input files', type=argcomplete.completers.DirectoryCompleter())
+    parser.add_argument('-o', '--output-file', default='output.gpx', help='Output file name', type=argparse.FileType('w'))
     parser.add_argument('-m', '--enable-metadata', action='store_true', help='Enable metadata in the output file')
     parser.add_argument('-c', '--enable-coloring', action='store_true', help='Enable coloring in the output file')
+
+    # Activate autocompletion for paths
+    argcomplete.autocomplete(parser)
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -19,7 +23,7 @@ def main():
     # Create an instance of GPXConcatenator with the specified parameters
     concatenator = GPXConcatenator(
         input_files,
-        args.output_file,
+        args.output_file.name,
         enable_metadata=args.enable_metadata,
         enable_coloring=args.enable_coloring
     )
